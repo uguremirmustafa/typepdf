80713e17c30f7a418280d8a65c1c99199ee88d72
uguremirmustafa <uguremirmustafa@gmail.com>
2021-05-26 18:24:40 +0300

    solved rerendering issue

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This is an example repo for the usage of [React-PDF.](https://react-pdf.org/) It simply takes the user name and prints it in a pre-customised PDF file which can be downloaded by the user.
 
+Visit the [live application.](https://typepdf.vercel.app)
+
 ### Tech
 
 - NextJS
@@ -8,5 +10,5 @@ This is an example repo for the usage of [React-PDF.](https://react-pdf.org/) It
 
 ### Todo
 
-- Prevent re-rendering of the component
-- Add some useful input fields via a library like React-hook-form and customise the pdf template.
+- [x] Prevent re-rendering of the component
+- [ ] Add some useful input fields via a library like React-hook-form and customise the pdf template.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import PdfDocument from '../components/File';
+import { useForm } from 'react-hook-form';
+
+type FormValues = {
+  name: string;
+};
 
 const Home = () => {
   const [name, setName] = useState('');
   const [isClient, setIsClient] = useState(false);
-  console.log(name);
 
   useEffect(() => {
     setIsClient(true);
   }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+    setName(data.name);
+  };
+  const renderDownloadButton = useMemo(
+    () => (
+      <PDFDownloadLink
+        document={<PdfDocument name={name} />}
+        fileName="file.pdf"
+        style={{
+          textDecoration: 'none',
+          padding: '10px',
+          color: '#4a4a4a',
+          backgroundColor: '#f2f2f2',
+          border: '1px solid #4a4a4a',
+        }}
+        className="btn"
+      >
+        {({ blob, url, loading, error }) =>
+          loading ? 'Loading document...' : 'Your pdf is ready. Download!'
+        }
+      </PDFDownloadLink>
+    ),
+    [name]
+  );
+
   return (
     <div className="home">
-      <input
-        placeholder="enter your name and click download"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name" style={{ display: 'block' }}>
+          Name
+        </label>
+        <input
+          {...register('name', {
+            required: 'This field is required',
+            maxLength: { value: 200, message: 'You exceeded the length' },
+          })}
+          type="text"
+          id="name"
+        />
+        {errors.name && <p>{errors.name.message}</p>}
+        <button className="btn" type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'submitting' : 'Save changes to pdf'}
+        </button>
+      </form>
 
-      {isClient && (
-        <PDFDownloadLink
-          document={<PdfDocument name={name} />}
-          fileName="file.pdf"
-          style={{
-            textDecoration: 'none',
-            padding: '10px',
-            color: '#4a4a4a',
-            backgroundColor: '#f2f2f2',
-            border: '1px solid #4a4a4a',
-          }}
-        >
-          {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download Pdf')}
-        </PDFDownloadLink>
-      )}
+      {isClient && name !== '' && renderDownloadButton}
     </div>
   );
 };
